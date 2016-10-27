918ccb288bff1d3eb65aea0cc7b098c88fa7d7c2
Garrett Wong <garrett.wong@wdc.com>
2016-10-27 15:40:01 -0700

    initial commit, post and get and auth functionality

---
+++ read-write.js
@@ -0,0 +1,64 @@
+// https://firebase.google.com/docs/database/web/read-and-write
+function readData(userId) {
+    return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
+        console.log(snapshot.val());
+
+        // render
+        $('#userList').html('<li>' + JSON.stringify(snapshot.val()) + '</li>');
+    });
+}
+
+function readList() {
+    return firebase.database().ref('/userposts').once('value').then(function(snapshot) {
+        console.log(snapshot.val());
+
+        var val = snapshot.val();
+
+        var arr = [];
+        for(var prop in val) {
+            var obj = $.extend(val[prop], {_id: prop });
+            arr.push(obj);
+        }
+        console.log(arr);
+
+        // render
+        var html = '';
+        for(var idx in arr) {
+            var gg = '<li>' + JSON.stringify(arr[idx]) + '</li>';
+            html += gg;
+        }
+        $('#userList').append(html);
+    });
+}
+
+// update
+function writeData(userId, name, email, imageUrl) {
+    firebase.database().ref('users/' + userId).set({
+        username: name,
+        email: email,
+        profile_picture : imageUrl
+    });
+}
+
+// add new
+function writeNewUser(uid, username, picture, title, body) {
+  // A post entry.
+  var postData = {
+    author: username,
+    uid: uid,
+    body: body,
+    title: title,
+    starCount: 0,
+    authorPic: picture
+  };
+
+  // Get a key for a new Post.
+  var newPostKey = firebase.database().ref().child('userposts').push().key;
+
+  // Write the new post's data simultaneously in the posts list and the user's post list.
+  var updates = {};
+  updates['/userposts/' + newPostKey] = postData;
+  //updates['/user-posts/' + uid + '/' + newPostKey] = postData;
+
+  return firebase.database().ref().update(updates);
+}
