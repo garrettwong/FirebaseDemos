9611f8cefa5610ce823922301a3884126c983b0b
Garrett Wong <garrett.wong@wdc.com>
2016-11-02 09:29:04 -0700

    add user auth getting

--- js/fbRef.js
+++ js/fbRef.js
@@ -3,6 +3,8 @@ angular.module('app').factory('fbRef', function(auth) {
         // child references: https://firebase.google.com/docs/reference/js/firebase.database.Reference
 
         getPreferencesRef: function () {
+            console.log(auth.$getAuth()); // gets the user
+
             return firebase.database().ref('preferences').child(auth.$getAuth().uid);
             //new Firebase('https://../preferences' + auth.$getAuth().uid);
         },
---
+++ no-angular/firebase-init.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCJ2bRLYCVApC8Nnf3j5FoaT46YX-9wdLA",
+    authDomain: "cat-me-if-you-can.firebaseapp.com",
+    databaseURL: "https://cat-me-if-you-can.firebaseio.com",
+    storageBucket: "cat-me-if-you-can.appspot.com",
+    messagingSenderId: "948203674523"
+};
+
+firebase.initializeApp(config);
